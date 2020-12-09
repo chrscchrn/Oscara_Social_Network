131eb5220aef01343955ae2bf3c6858870347ea4
chrscchrn <cochranschris@gmail.com>
2020-12-09 13:34:13 -0600

    fixing http redirect error

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(express.static('uploads'));
 
 app.use((req, res, next) => {
   if (process.env.NODE_ENV === 'production') {
-      if (req.headers.host === 'oscara.herokuapp.com')
-          return res.redirect(301, 'https://www.oscara.herokuapp.com');
       if (req.headers['x-forwarded-proto'] !== 'https')
           return res.redirect('https://' + req.headers.host + req.url);
       else
