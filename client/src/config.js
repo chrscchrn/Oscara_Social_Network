2f14638eeb53ba4f8e5739c5d616a155f88b16e6
chrscchrn <cochranschris@gmail.com>
2021-02-24 12:56:14 -0600

    removing build and editing for heroku config vars

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@ import configJson from "./auth_config.json";
 export function getConfig() {
 
     const audience = process.env.audience || 
-        configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER"
+        (configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER")
             ? configJson.audience
             : null;
 
